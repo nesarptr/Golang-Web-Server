d59e58bfb096f36e7e42e9dcd43ba2617654f20b
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2022-12-06 14:47:17 +0600

    web server created

--- src/main.go
+++ src/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Printf("Starting Web Server at port 8080\n")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
